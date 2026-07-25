a8b43f7998fc5e48ba73289e2e4a1f12b3750b4c
Yoan Ivanov <yoan_ivanov1@yahoo.com>
2026-07-25 19:01:16 +0300

    build: add users and playlists apps for api routing

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'core',
+    'user',
+    'playlist',
+
     'rest_framework',
     'rest_framework.authtoken',
     'drf_spectacular',
---
+++ backend/app/playlist/views.py
@@ -0,0 +1,3 @@
+"""
+Views for playlist-related operations.
+"""
